093bd8225289ba4aa2f2f949f50b4c4bb62f075e
Dustin Irving <dustin.irving@gmail.com>
2023-12-17 13:05:26 -0500

    chore: test passing input params to deploy

--- .github/actions/deploy/index.js
+++ .github/actions/deploy/index.js
@@ -1,5 +1,10 @@
+const core = require('@actions/core');
+
 function main() {
-  console.log('Deploy workflow running');
+  const s3Bucket = core.getInput('s3Bucket');
+  console.log('s3 bucket: ', s3Bucket);
+  const cfDistributionId = core.getInput('cfDistributionId');
+  console.log('cf distribution id: ', cfDistributionId);
 }
 
 main();
